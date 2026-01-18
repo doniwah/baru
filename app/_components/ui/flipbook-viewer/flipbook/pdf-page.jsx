e1e56c8724bd7c3a3de8f2cdcbef63d141028583
doniwah <whyddoni@gmail.com>
2026-01-18 15:38:15 +0700

    upgre 4

--- app/_components/ui/flipbook-viewer/flipbook/pdf-page.jsx
+++ app/_components/ui/flipbook-viewer/flipbook/pdf-page.jsx
@@ -4,7 +4,14 @@ import { Page } from "react-pdf";
 
 const PdfPage = forwardRef(({ page, height, zoomScale, isPageInView, isPageInViewRange }, ref) => {
     return (
-        <div ref={ref} className="bg-transparent">
+        <div 
+            ref={ref} 
+            className="bg-transparent"
+            style={{
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3), 0 6px 20px rgba(0, 0, 0, 0.19)',
+                borderRadius: '2px'
+            }}
+        >
             {isPageInViewRange && (
                 <Page
                     devicePixelRatio={(isPageInView && zoomScale > 1.7) ? Math.min(zoomScale * window.devicePixelRatio, 5) : window.devicePixelRatio}
